c3adf2c73ed48699987b140b5573c969477d6bdd
Andre G B Farias <andreriffen6@gmail.com>
2024-11-02 19:57:24 -0300

    .

--- config/database.php
+++ config/database.php
@@ -69,7 +69,7 @@ return [
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'tallcms'),
+            'database' => env('DB_DATABASE', 'laravel'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
